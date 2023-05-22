89de33db08e69a71c1180407f80c84b9cb47c253
Amr Aly <amr.aly57@yahoo.com>
2023-05-22 08:27:00 +0300

    Created NowPlaying page object including locators for tabs

---
+++ Appium-ios-MoviesApp/src/main/java/com/amr/automation/pageObjects/NowPlaying.java
@@ -0,0 +1,49 @@
+package com.amr.automation.pageObjects;
+
+import com.amr.automation.utils.IOSActions;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class NowPlaying extends IOSActions {
+
+    public IOSDriver driver;
+
+    public NowPlaying(IOSDriver driver) {
+        super(driver);
+        this.driver = driver;
+        PageFactory.initElements(new AppiumFieldDecorator(driver),this);
+    }
+
+    // Locators
+    @iOSXCUITFindBy(accessibility="Now Playing")
+    private WebElement nowPlayingTab;
+
+    @iOSXCUITFindBy(accessibility="Popular")
+    private WebElement popularTab;
+
+    @iOSXCUITFindBy(accessibility="Upcoming")
+    private WebElement upcomingTab;
+
+    @iOSXCUITFindBy(accessibility="Top Rated")
+    private WebElement topRatedTab;
+
+
+    public void goToNowPlayingTab() {
+        nowPlayingTab.click();
+    }
+
+    public void goToPopularTab() {
+        popularTab.click();
+    }
+
+    public void goToUpcomingTab() {
+        upcomingTab.click();
+    }
+
+    public void goToTopRatedTab() {
+        topRatedTab.click();
+    }
+}
