1a2b09c6e52a9c902f217fe5e893bb8040e131a9
Amr Aly <amr.aly57@yahoo.com>
2023-05-22 08:25:40 +0300

    Created IOS Actions Utils

---
+++ Appium-ios-MoviesApp/src/main/java/com/amr/automation/utils/IOSActions.java
@@ -0,0 +1,26 @@
+package com.amr.automation.utils;
+
+import io.appium.java_client.ios.IOSDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IOSActions {
+
+    IOSDriver driver;
+
+    public IOSActions(IOSDriver driver) {
+        this.driver = driver;
+    }
+
+    // Long Press Action
+    public void longPress(WebElement el) {
+        Map<String, Object> params = new HashMap<>();
+        params.put("element", ((RemoteWebElement) el).getId());
+        params.put("duration", 3);
+
+        driver.executeScript("mobile:touchAndHold", params);
+    }
+}
